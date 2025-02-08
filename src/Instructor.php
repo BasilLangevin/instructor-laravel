8efa0ed64dfbbae6675670cdf233390d174aafe3
BasilLangevin <basil.langevin@gmail.com>
2025-02-08 09:06:51 -0800

    feat: forward inaccessible Instructor calls to Prism

--- src/Instructor.php
+++ src/Instructor.php
@@ -6,12 +6,19 @@ use EchoLabs\Prism\Structured\PendingRequest;
 
 class Instructor
 {
-    public function __construct(
-        protected PendingRequest $prism,
+    public function __construct(protected PendingRequest $request,
     ) {}
 
     public static function make(): self
     {
         return app(Instructor::class);
+    }
+
+    /**
+     * Pass inaccessible method calls to the underlying Prism instance.
+     */
+    public function __call(mixed $method, mixed $arguments): mixed
+    {
+        return $this->request->$method(...$arguments);
     }
 }
--- src/InstructorTest.php
+++ src/InstructorTest.php
@@ -5,6 +5,26 @@ use BasilLangevin\InstructorLaravel\Instructor;
 
 covers(Instructor::class);
 
+beforeEach(function () {
+    $this->instructor = InstructorFacade::make();
+
+    $reflection = new ReflectionClass($this->instructor);
+    $property = $reflection->getProperty('request');
+    $property->setAccessible(true);
+
+    $this->request = $property->getValue($this->instructor);
+});
+
 it('can be created via its facade')
     ->expect(fn () => InstructorFacade::make())
     ->toBeInstanceOf(Instructor::class);
+
+it('can pass method calls to the underlying Prism instance', function () {
+    $this->instructor->withPrompt('Hello, world!');
+
+    $reflection = new ReflectionClass($this->request);
+    $property = $reflection->getProperty('prompt');
+    $property->setAccessible(true);
+
+    expect($property->getValue($this->request))->toBe('Hello, world!');
+});
