64237495ae58957b9ee9b9ca87b608885e6dedae
BasilLangevin <basil.langevin@gmail.com>
2025-02-08 08:46:22 -0800

    chore: run Pint

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace BasilLangevin\InstructorLaravel\Tests;
 
+use BasilLangevin\InstructorLaravel\InstructorLaravelServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use BasilLangevin\InstructorLaravel\InstructorLaravelServiceProvider;
 
 class TestCase extends Orchestra
 {
