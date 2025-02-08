06cd7e7b6ef0c487fe118fc04617b972e33e1d85
BasilLangevin <basil.langevin@gmail.com>
2025-02-08 09:08:40 -0800

    chore: remove unused config and command

--- src/InstructorLaravelServiceProvider.php
+++ src/InstructorLaravelServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace BasilLangevin\InstructorLaravel;
 
-use BasilLangevin\InstructorLaravel\Commands\InstructorLaravelCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +9,8 @@ class InstructorLaravelServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('instructor-laravel')
-            ->hasConfigFile('instructor')
-            ->hasViews()
-            ->hasMigration('create_instructor_laravel_table')
-            ->hasCommand(InstructorLaravelCommand::class);
+            ->hasConfigFile('instructor');
     }
 }
