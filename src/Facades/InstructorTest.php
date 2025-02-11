8cdd15c05fafce09f1fda5ca140750ffac377061
BasilLangevin <basil.langevin@gmail.com>
2025-02-11 13:52:57 -0800

    tests: add test covering Instructor fakes

--- src/Facades/InstructorTest.php
+++ src/Facades/InstructorTest.php
@@ -2,6 +2,8 @@
 
 use BasilLangevin\InstructorLaravel\Facades\Instructor as InstructorFacade;
 use BasilLangevin\InstructorLaravel\Instructor;
+use BasilLangevin\InstructorLaravel\Tests\Support\Data\BirdData;
+use BasilLangevin\InstructorLaravel\Tests\Support\InstructorFake;
 
 covers(InstructorFacade::class);
 
@@ -9,4 +11,18 @@ it('is a facade for the Instructor class', function () {
     $instructor = InstructorFacade::make();
 
     expect($instructor)->toBeInstanceOf(Instructor::class);
+});
+
+it('can be faked', function () {
+    $fake = InstructorFacade::fake(['species' => 'Western Tanager']);
+
+    $instructor = InstructorFacade::make()->withSchema(BirdData::class);
+
+    $result = $instructor->generate();
+
+    expect($result)->toBeInstanceOf(BirdData::class);
+    expect($result->species)->toBe('Western Tanager');
+
+    expect($fake)->toBeInstanceOf(InstructorFake::class);
+    expect($fake->requests())->toHaveCount(1);
 });
